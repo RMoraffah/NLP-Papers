fbf47faf47d49e30f0a47b166886416fa8fde261
[ Ujun Jeong - Unknown - Graduate Student - huanliu ] <ujeong1@agave1.agave.rc.asu.edu>
2020-10-24 23:55:15 -0700

    Ujun upadated

--- Codes/coreference_code/Narrative_QA_Generation/Code_question_backtranslation/back_translation.py
+++ Codes/coreference_code/Narrative_QA_Generation/Code_question_backtranslation/back_translation.py
@@ -2,7 +2,9 @@ import json
 from googletrans import Translator                                              
                                                                                 
 trans = Translator()                                                            
+
 quest = [] 
+'''
 filename="data/train.json"
 with open(filename, "r") as r:
     input_data = json.load(r)
@@ -15,13 +17,13 @@ with open("train_q.txt", "w") as f:
         en = trans.translate(fr.text, src='fr', dest='en')                              
         print(en.text)
         f.write(en.text+"\n\n\n")
-
+'''
 filename="data/valid.json"
 quest = []
 with open(filename, "r") as r:
     input_data = json.load(r)
     for para in input_data:
-        trans.append(para["Question"])
+        quest.append(para["Question"])
 
 with open("valid_q.txt", "w") as f:
     for para in quest:
